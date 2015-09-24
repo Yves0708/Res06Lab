eac01ffdcb60aaca99918f7a82b150e6714fa2f1
Yves0708 <yvesloveisis@gmail.com>
2015-09-24 16:56:27 +0800

    Res06Lab2.6

--- gen/net/cloud95/android/lession/res06/R.java
+++ gen/net/cloud95/android/lession/res06/R.java
@@ -30,10 +30,15 @@ public final class R {
         public static final int latest=0x7f020002;
     }
     public static final class id {
+        public static final int btEnter=0x7f060001;
+        public static final int etName=0x7f060002;
+        public static final int sbSize=0x7f060004;
         public static final int textView1=0x7f060000;
+        public static final int textView2=0x7f060003;
     }
     public static final class layout {
         public static final int activity_res06=0x7f030000;
+        public static final int main_activity=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
