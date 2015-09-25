bc4c8528df03697a0c4741dc593c4e4fdb0dfb7a
Yves0708 <yvesloveisis@gmail.com>
2015-09-25 14:54:05 +0800

    Res06Lab2.8

--- gen/net/cloud95/android/lession/res06/R.java
+++ gen/net/cloud95/android/lession/res06/R.java
@@ -30,6 +30,7 @@ public final class R {
         public static final int banana=0x7f020002;
         public static final int ic_launcher=0x7f020003;
         public static final int latest=0x7f020004;
+        public static final int love=0x7f020005;
     }
     public static final class id {
         public static final int btEnter=0x7f060004;
